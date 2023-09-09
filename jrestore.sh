6c9fc7b8862a29ccd7559aae86b6426edf7e7a4c
Martin Kopp <kopp05@infotech.ch>
2023-09-09 17:05:50 +0200

    Split into Release Tools and Dev Tools

--- jrestore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-dbuser=c2u3
-db=c2j3
-dbpw=cdxXfugCSAJ9#
-mysql $db -u $dbuser -p$dbpw <prefixedtable_dump.sql
--- release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-VERSION_FILE='joomla-cms/libraries/src/Version.php'
-MAJOR_VERSION=$(php -r "define('JOOMLA_PLATFORM','Yes'); require('$VERSION_FILE'); $version = new Version; echo $version->MAJOR_VERSION;")
-echo $MAJOR_VERSION
