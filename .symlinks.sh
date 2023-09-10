589b76c02f0a26ca1cb2f4088f0e098f32de1013
Martin Kopp <kopp05@infotech.ch>
2023-09-10 19:50:58 +0200

    Script for building symlinks in parent directory

---
+++ .symlinks.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+#Build Symlinks to all .sh Files to the parent directory
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+echo $SCRIPT_DIR
+
+for script in *.sh
+do
+    rm -f ../$script
+    ln -s $SCRIPT_DIR/$script ../$script
+done
