1250a2285f464344d038c3f81601cb85c649a291
MacJoom <kopp05@infotech.ch>
2023-09-19 14:22:14 +0200

    New buildfromcheckout.sh

---
+++ buildfromcheckout.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+. globals.sh
+echo "Repository: $repository"
+echo "Checkout: $checkout"
+echo "Branch: $branch"
+echo "Tag: $tag"
+echo "Commit: $commit"
+echo "Next: $next"
+read -p "Press any key to start..."
+export GPG_TTY=$(tty)
+cd joomla-cms
+git checkout $checkout
+read -p "Press any key to create branch ..."
+git branch $branch
+git switch $branch
+cp ../config.joomla .git/config
+read -p "Press any key to push branch ..."
+git push --set-upstream origin $branch
+read -p "Press any key to start build/bump ..."
+php build/bump.php -v $tag -c $codename -d "$reldate"
+git commit -am "$commit"
+git tag -s -m "$commit" $tag
+read -p "Press any key to start build/build ..."
+php build/build.php
