c88ee6cacaa7bc1323b4c539f52f09d721e06802
Martin Kopp <kopp05@infotech.ch>
2025-03-04 18:03:05 +0100

    Joomla 4.4.11

--- reverttodevandpush.sh
+++ reverttodevandpush.sh
@@ -3,8 +3,8 @@
 export GPG_TTY=$(tty)
 cd joomla-cms
 read -p "Press 'enter' to build revert to dev ..."
-php build/bump.php -v $next-dev -d "$revdate"
-#php build/bump.php -v 4.4.10-dev -d "$revdate"
+#php build/bump.php -v $next-dev -d "$revdate"
+php build/bump.php -v 4.4.12-dev -d "$revdate"
 git add .
 read -p "Press any to commit ..."
 git commit -s -m "Revert to dev"
